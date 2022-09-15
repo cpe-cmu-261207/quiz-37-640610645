22174ea84bf0604e96e3585d48b4f92d7c98d354
640610645 <nontapan_chanadee@cmu.ac.th>
2022-09-15 14:51:11 +0700

    quiz

--- components/Reply.jsx
+++ components/Reply.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { comments } from "../libs/comments";
 
 export default function Reply(props) {
-  return <div></div>;
+  return (
+    <div>
+      {comments.map((x, i) => (
+        <div className="d-flex gap-2 my-2 ps-5" key={x.replies.username}>
+          <img
+            src={x.replies.userImagePath}
+            width="48"
+            height="48"
+            className="rounded-circle"
+            style={{ objectFit: "cover" }}
+          />
+          <div
+            className="rounded rounded-3 p-2"
+            style={{ backgroundColor: "#E5E7EB" }}
+          >
+            <span className="fw-semibold">{x.replies.username}</span>
+            <br />
+            <span>{x.replies.replyText}</span>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
